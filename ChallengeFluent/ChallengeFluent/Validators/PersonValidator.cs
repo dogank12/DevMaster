c2242563e069afdbf5fcc817b760144ee181473a
Doğan <Dogan.Kartaltepe@hotmail.com>
2020-05-13 16:10:17 -0500

    UI updates, adding Fluent reference, adding validator

---
+++ ChallengeFluent/ChallengeFluent/Validators/PersonValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeFluent.Validators
+{
+    public class PersonValidator: AbstractValidator<PersonModel>
+    {
+    }
+}
